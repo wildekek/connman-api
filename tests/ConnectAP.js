b01b56c9adfed08f8c8771cbf541fdf9127ec240
peteruithoven <peter@peteruithoven.nl>
2015-01-08 12:39:16 +0100

    Removed findAccessPoint
    (Not really necessary, not according to API)

--- tests/ConnectAP.js
+++ tests/ConnectAP.js
@@ -1,90 +1,108 @@
 var ConnMan = require('../');
+var async = require('async');
+var targetNetwork = 'hss'; //'Doodle3D-wisp';
 
 var connman = new ConnMan();
-connman.init(function() {
+var wifi; 
+var serviceData;
+var service;
 
-	var wifi = connman.technologies['WiFi'];
+async.series([
+    function(next) {
+        connman.init(next);
+    },
+    function(next) {
+        // get wifi technology object
+        wifi = connman.technologies.WiFi; 
+        console.log("perform a scan, just to make sure we have the latest");
+        wifi.scan(next); 
+    },
+    function(next) {
+        // get available networks
+        console.log("find target network");
+        wifi.getServices(function(err, services) {
+            if(err) return next(err);
+            for(var serviceName in services) {
+                if(services[serviceName].Name == targetNetwork) {
+                    serviceData = services[serviceName];
+                    console.log("found network '"+targetNetwork+"'");
+                    next();
+                    break;
+                }
+            }
+            if (!serviceData) {
+                return next(new Error("Network '"+targetNetwork+"' not found"));
+            }
+        });
+    },
+    function(next) {
+        // get wifi service object
+        connman.getService(serviceData.serviceName, function(err, ser) {
+            service = ser;
+            next(err);
+        });
+    },
+    function(next) {
+        // Connect to that service
+        console.log('Connecting ...');
+        service.connect(function(err, agent) {
+            if (err) return next(err); 
+            var failed = false;
+            agent.on('Release', function() {
+                console.log('Release');
+            });
+            agent.on('ReportError', function(path, err) {
+                console.log('ReportError:');
+                console.log(err);
+                failed = true;
+                /* connect-failed */
+                /* invalid-key */
+            });
+            agent.on('RequestBrowser', function(path, url) {
+                console.log('RequestBrowser');
+            });
+            /* Initializing Agent for connecting access point */
+            agent.on('RequestInput', function(path, dict, callback) {
+                console.log(dict);
 
-	wifi.findAccessPoint('Fred', function(err, service) {
-		if (!service) {
-			console.log('No such access point');
-			process.exit();
-			return;
-		}
+                if ('Passphrase' in dict) {
+                    callback({ 'Passphrase': '12345' });
+                    return;
+                }
 
-		connman.getConnection(service.serviceName, function(err, ap) {
-
-			/* Making connection to access point */
-			ap.connect(function(err, agent) {
-
-				if (err)
-					return;
-
-				var failed = false;
-
-				console.log('Connecting ...');
-
-				agent.on('Release', function() {
-					console.log('Release');
-				});
-
-				agent.on('ReportError', function(path, err) {
-					console.log('ReportError:');
-					console.log(err);
-					failed = true;
-					/* connect-failed */
-					/* invalid-key */
-				});
-
-				agent.on('RequestBrowser', function(path, url) {
-					console.log('RequestBrowser');
-				});
-
-				/* Initializing Agent for connecting access point */
-				agent.on('RequestInput', function(path, dict, callback) {
-					console.log(dict);
-
-					if ('Passphrase' in dict) {
-						callback({ 'Passphrase': '12345' });
-						return;
-					}
-
-					callback({});
-				});
-
-				agent.on('Cancel', function() {
-					console.log('Cancel');
-				});
-
-				ap.on('PropertyChanged', function(name, value) {
-					console.log(name + '=' + value);
-
-					if (name == 'State') {
-						switch(value) {
-						case 'failure':
-							console.log('Connection failed');
-							break;
-
-						case 'association':
-							console.log('Associating ...');
-							break;
-
-						case 'configuration':
-							console.log('Configuring ...');
-							break;
-
-						case 'online':
-							console.log('Connected');
-							process.exit();
-							break;
-						}
-					}
-
-				});
-			});
-
-		});
-	});
-
-	
+                callback({});
+            });
+            agent.on('Cancel', function() {
+                console.log('Cancel');
+            });
+            next();
+        });
+    },
+    function(next) {
+        // listen for service property changes
+        service.on('PropertyChanged', function(name, value) {
+            console.log(name + '=' + value);
+            if (name == 'State') {
+                switch(value) {
+                case 'failure':
+                    next(new Error('Connection failed'));
+                    break;
+                case 'association':
+                    console.log('Associating ...');
+                    break;
+                case 'configuration':
+                    console.log('Configuring ...');
+                    break;
+                case 'online':
+                case 'ready':
+                    console.log('Connected');
+                    next();
+                    break;
+                }
+            }
+        });
+    },
+],function(err) {
+    console.log("connect sequence finished ",err || '');
+    process.exit();
 });
